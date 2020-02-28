0f7dbd34aea24de38f335cc364801a59242448dd
asef18766 <asef18766@gmail.com>
2020-02-28 20:02:47 +0800

    fix large file error

--- src/utils/RunCmd.go
+++ src/utils/RunCmd.go
@@ -3,38 +3,20 @@ package utils
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 //RunCmd run a specify command and get its stderr as return
 //note: it's a blocking function and non-thread safe
 func RunCmd(cmd string, opts []string) string {
-	opts = append([]string{cmd}, opts...)
-	re, we, err := os.Pipe()
-	procArg := &os.ProcAttr{
-		Files: []*os.File{os.Stdin, we, os.Stderr},
-	}
-
-	fmt.Printf("RunCmd execute cmd:%s %v\n", cmd, opts)
-	proc, err := os.StartProcess(cmd, opts, procArg)
-
+	cmdobj := exec.Command(cmd, opts...)
+	println("running cmd:")
+	fmt.Printf("%+v %+v\n", cmd, opts)
+	cmdobj.Stdin = os.Stdin
+	res, err := cmdobj.Output()
 	if err != nil {
-		print("fork err:", err.Error())
+		println("errror when getting cmd output:", err.Error())
 	}
-	proc.Wait()
-	if re == nil {
-		return ""
-	}
-	var result string = ""
-	for {
-		var buf = make([]byte, 65525)
-		num, err := re.Read(buf)
-		if err != nil {
-			print("read err:", err.Error())
-		}
-		result += string(buf)
-		if num < 65525 {
-			break
-		}
-	}
-	return result
+	println("result\n", string(res))
+	return string(res)
 }
